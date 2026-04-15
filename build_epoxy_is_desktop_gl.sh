b04fdf4d44a8e6e813c6ebd1fa4ffd1d2e05c80c
UMU618 <umu618@hotmail.com>
2026-04-15 16:24:32 +0800

    + epoxy_is_desktop_gl

---
+++ build_epoxy_is_desktop_gl.sh
@@ -0,0 +1,1 @@
+gcc epoxy_is_desktop_gl.c -lepoxy -o epoxy_is_desktop_gl
---
+++ epoxy_is_desktop_gl.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include <epoxy/gl.h>
+
+int main() {
+  printf("epoxy_is_desktop_gl: %d\n", epoxy_is_desktop_gl());
+}
